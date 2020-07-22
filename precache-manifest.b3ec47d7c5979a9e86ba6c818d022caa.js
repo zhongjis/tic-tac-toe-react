28f01b66616ae399be6e1a835c0713696350fab8
Zhongjie Shen <zhongjis@uci.edu>
2020-07-21 20:49:45 -0700

    Updates

---
+++ precache-manifest.b3ec47d7c5979a9e86ba6c818d022caa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9634d33befb1b9f88000998e159758eb",
+    "url": "/tic-tac-toe-react/index.html"
+  },
+  {
+    "revision": "b5a790c478aba8d09b76",
+    "url": "/tic-tac-toe-react/static/css/main.c95da0b3.chunk.css"
+  },
+  {
+    "revision": "c1659b7f14bde634615b",
+    "url": "/tic-tac-toe-react/static/js/2.7778d4e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-react/static/js/2.7778d4e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5a790c478aba8d09b76",
+    "url": "/tic-tac-toe-react/static/js/main.ec8b7a87.chunk.js"
+  },
+  {
+    "revision": "b3fb02dc9df6095594b3",
+    "url": "/tic-tac-toe-react/static/js/runtime-main.81226336.js"
+  }
+]);
